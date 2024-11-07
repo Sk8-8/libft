189c09d11460b34ba461120880af6e28fb5372f0
Kerloye Guillemin <guilleminkerloye@gmail.com>
2024-11-07 16:01:39 +0100

    first commit

---
+++ ft_isalnum.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 14:30:01 by kguillem          #+#    #+#             */
+/*   Updated: 2024/11/07 13:48:25 by kguillem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) == 1 || ft_isdigit(c) == 1)
+		return (1);
+	else
+		return (0);
+}
+
+#include <unistd.h>
+int	main(void)
+{
+	int	i;
+
+	i = ft_isalnum('c');
+	if (i == 1)
+		write(1, "ouiouioui", 9);
+	else
+		write(1, "nuhuh", 9);
+	return (0);
+}
