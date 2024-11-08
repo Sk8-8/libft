6ad630b77e96d30c9634c43994a97594182c0ba6
Kerloye Guillemin <guilleminkerloye@gmail.com>
2024-11-08 13:26:23 +0100

    8 Nov : ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,11 +6,30 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:34:15 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/05 17:23:30 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/08 13:24:23 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_bzero(void *s, size_t n)
-{
+#include <strings.h>
 
+void	ft_bzero(void *s, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i != n)
+	{
+		((unsigned char *)s)[i] = '\0';
+		i++;
+	}
 }
+/*
+#include <stdio.h>
+int	main(void)
+{
+	char	str[]  = "test";
+	ft_bzero(str, 4);
+	printf("%p\n", str);
+	printf("%s\n", str);
+	return (0);
+}*/
