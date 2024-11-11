47fe4cb90de66a387f1625d5bd677209f94d3740
Kerloye Guillemin <kguillem@c2r2p2.42perpignan.fr>
2024-11-11 19:31:03 +0100

    ft_memchr.c Done 11/11/2024

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,11 +6,31 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:59:51 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/05 18:00:49 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/11 19:27:40 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*memchr(const void *s, int c, size_t n)
-{
+#include <string.h>
 
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (((unsigned char *)s)[i] != c && i < n)
+		i ++;
+	if (((unsigned char *)s)[i] == c)
+		return ((void *)s + i);
+	else
+		return (NULL);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%p\n", ft_memchr("test", 's', 1));
+	printf("%p\n", ft_memchr("test", 's', 3));
+	printf("%p\n", ft_memchr("test", 'z', 1));
+	return (0);
+}*/
--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:28:09 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/11 17:32:01 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/11 19:02:22 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	a
+	unsigned char	res;
+
+	if (n ==0)
+		return (0);
+	res = ((unsigned char) s1[n]) - ((unsigned char) s2[n]);
+	return (res);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_memcmp("fail", "goal", 0));
+	printf("%d\n", ft_memcmp("fail", "goal", 1));
+	printf("%d\n", ft_memcmp("fail", "goal", 2));
+	printf("%d\n", ft_memcmp("fail", "goal", 3));
+	printf("%d\n", ft_memcmp("fail", "goal", 4));
+	return (0);
 }
