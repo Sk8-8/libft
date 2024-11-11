085abe76501b05056bde66746e1397e8b9cc8afa
Kerloye Guillemin <kguillem@c2r2p2.42perpignan.fr>
2024-11-11 17:26:00 +0100

    ft_atoi done

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:59:38 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/11 15:59:30 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/11 17:24:51 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,11 @@ int	ft_atoi(const char *nptr)
 {
 	int	sign;
 	int	i;
+	int	res;
 
 	sign = 1;
 	i = 0;
+	res = 0;
 	if (nptr[0] == '-')
 	{
 		sign = -1;
@@ -26,5 +28,21 @@ int	ft_atoi(const char *nptr)
 	}
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-
+		res = (res * 10) + (nptr[i] - 48);
+		i ++;
+	}
+	if (nptr[i] == '\0')
+		return (res * sign);
+	else
+		return (0);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%d\n", ft_atoi("-123"));
+	printf("%d\n", ft_atoi("321"));
+	printf("%d\n", ft_atoi("test"));
+	return (0);
+}*/
