5e503f7664bf2f1c69350c11d4e0a63c8f299a73
Kerloye Guillemin <kguillem@c2r2p2.42perpignan.fr>
2024-11-11 18:34:06 +0100

    ft_strnstr.c done 11/11/2024

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,11 +6,41 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 18:53:32 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/05 18:56:26 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/11 18:32:01 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	j = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] != '\0' && i < len && little[j] != '\0')
+	{
+		if (big[i] == little[j])
+			j ++;
+		else
+			j = 0;
+		i ++;
+	}
+	if (little[j] == '\0')
+		return ((char *)&big[i - j]);
+	else
+		return (NULL);
 }
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("%p\n", ft_strnstr("testing", "test", 3));
+	printf("%p\n", ft_strnstr("testing", "ting", 7));
+	printf("%p\n", ft_strnstr("testing", "", 3));
+	return (0);
+}*/
