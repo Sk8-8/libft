2e93dcb987eaeb6a13caff1a99dd56b5484ad967
Kerloye Guillemin <kguillem@c1r3p10.42perpignan.fr>
2024-11-22 19:12:46 +0100

    22/11/2024 ft_split progress

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 17:02:07 by kguillem          #+#    #+#             */
-/*   Updated: 2024/11/16 18:41:50 by kguillem         ###   ########.fr       */
+/*   Updated: 2024/11/22 19:11:53 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,60 +15,50 @@
 size_t	count(char const *s, char c)
 {
 	size_t	i;
-	size_t	j;
+	size_t	ccount;
 
 	i = 0;
-	j = 0;
-	while (s[i] != '\0')
+	ccount = 0;
+	while (i != '\0')
 	{
 		if (s[i] == c)
-			j ++;
+			ccount ++;
 		i ++;
 	}
-	return (j);
+	return (ccount);
+}
+
+size_t	cutter(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0' || s[i] != c)
+		i ++;
+	return (i);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	char	**tab;
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	char	**tab;
 
 	i = 0;
+	j = 0;
 	k = 0;
 	tab = malloc(sizeof(*tab) * (count(s, c) + 1));
 	if (tab == NULL)
 		return (NULL);
-	while (s[i] != '\0')
+	while (s[k] != '\0')
 	{
-		if (s[i] == c)
-		{
-			tab[k] = malloc(sizeof(**tab) * (i + 1 - j));
-			if (tab[k] == NULL)
-				return (NULL);
-			j = i;
-			k ++;
-		}
-		i ++;
+		tab[j] = malloc(sizeof(**tab) * (cutter(s + k, c)));
+		if (tab[j] == NULL)
+			return (NULL);
+		k = cutter(s + k, c);
+		j ++;
 	}
-	i = 0;
-	j = 0;
-	k = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			tab[k][i] = '\0';
-			k ++;
-			i ++;
-			j = i;
-		}
-		tab[k][i - j] = s[i];
-		i ++;
-	}
-	tab[k][i] = '\0';
-	tab[k + 1] = NULL;
 }
 
 #include <stdio.h>
